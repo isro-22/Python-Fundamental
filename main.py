04a30d2dbad4005752e54a1c9822bc483a695329
muhammadisro <moehammad.isro@gmail.com>
2024-09-01 23:41:24 +0700

    Solve problem of being programming meme, and making a program based on that case

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print ("Hello World")
+print ("One day, a mother asked her son to buy food.\n"
+       "Then, his finally went to the shop.")
+
+print ("The child asked, does it have milk (yes/no)?")
+command_A = input()
+
+conclusion = "Conclusion is His son come home and \nthe result is "
+
+if command_A=="yes":
+    print(conclusion + "His son asked again, do you have eggs (yes/no)?")
+    command_B = input()
+    if command_B=="yes":
+        print(conclusion + "His son bought 6 eggs and 1 bottle of milk in the end")
+    else:
+        print(conclusion + "His son only bought 1 bottle of milk")
+else:
+    print (conclusion + "Not buying anything")
